4718ecdcff86abcf7cd3f72c0ddbb149629065c5
cesardanziger-blip <cesar.danziger@gmail.com>
2026-07-01 16:28:08 -0300

    feat(infrastructure): implement repository classes

---
+++ src/OrderManagement.Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderManagement.Domain.Entities;
+using OrderManagement.Domain.Interfaces;
+using OrderManagement.Infrastructure.Context;
+
+namespace OrderManagement.Infrastructure.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly OrderManagementDbContext _context;
+
+        public CustomerRepository(OrderManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAsync(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Customer>> GetAllAsync()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+        public async Task<Customer?> GetByIdAsync(Guid id)
+        {
+            return await _context.Customers.FirstOrDefaultAsync(x => x.Id == id);
+        }
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/OrderManagement.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderManagement.Domain.Entities;
+using OrderManagement.Domain.Interfaces;
+using OrderManagement.Infrastructure.Context;
+
+namespace OrderManagement.Infrastructure.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly OrderManagementDbContext _context;
+
+        public OrderRepository(OrderManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAsync(Order order)
+        {
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Order>> GetAllAsync()
+        {
+            return await _context.Orders.ToListAsync();
+        }
+
+        public async Task<Order?> GetByIdAsync(Guid id)
+        {
+            return await _context.Orders.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task UpdateAsync(Order order)
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
