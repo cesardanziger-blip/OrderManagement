89cca31865816502f9d85afdf2bab8d2d7d4cc2b
cesardanziger-blip <cesar.danziger@gmail.com>
2026-07-01 19:53:34 -0300

    feat(application): implement product validator

---
+++ src/OrderManagement.Application/Validators/Products/CreateProductCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderManagement.Application.Validators.Products
+{
+    public class CreateProductCommand
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
---
+++ src/OrderManagement.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace OrderManagement.Application.Validators.Products
+{
+    public class CreateProductValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Product name is required.");
+
+            RuleFor(x => x.Price)
+                .GreaterThan(0)
+                .WithMessage("Price must be greater than zero.");
+
+            RuleFor(x => x.Stock)
+                .GreaterThanOrEqualTo(0)
+                .WithMessage("Stock cannot be negative.");
+        }
+    }
+}
