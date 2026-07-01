d8efbf0af679a4d9ef722528e974f1b387589f2c
cesardanziger-blip <cesar.danziger@gmail.com>
2026-07-01 20:20:24 -0300

    feat(API): add CustomerController and update appsettings

---
+++ src/OrderManagement.API/Controllers/CustomerController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderManagement.Application.Contracts.Requests;
+using OrderManagement.Application.Contracts.Responses;
+using OrderManagement.Application.Interfaces;
+
+namespace OrderManagement.API.Controllers
+{
+    [ApiController]
+    [Route("api/customers")]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        // POST: api/customers
+        [HttpPost]
+        public async Task<ActionResult<Guid>> Create([FromBody] CreateCustomerRequest request)
+        {
+            var id = await _customerService.CreateAsync(request);
+            return CreatedAtAction(nameof(GetById), new { id }, id);
+        }
+
+        // GET: api/customers/{id}
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<CustomerResponse>> GetById(Guid id)
+        {
+            var customer = await _customerService.GetByIdAsync(id);
+
+            if (customer is null)
+                return NotFound();
+
+            return Ok(customer);
+        }
+
+        // GET: api/customers
+        [HttpGet]
+        public async Task<ActionResult<List<CustomerResponse>>> GetAll()
+        {
+            var customers = await _customerService.GetAllAsync();
+            return Ok(customers);
+        }
+
+        // PATCH: api/customers/{id}/status
+        [HttpPatch("{id:guid}/status")]
+        public async Task<IActionResult> UpdateStatus(Guid id)
+        {
+            await _customerService.DeactivateAsync(id);
+            return NoContent();
+        }
+    }
+}
--- src/OrderManagement.API/Program.cs
+++ src/OrderManagement.API/Program.cs
@@ -1,6 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using OrderManagement.Application.Interfaces;
+using OrderManagement.Application.Services;
+using OrderManagement.Domain.Interfaces;
+using OrderManagement.Infrastructure.Context;
+using OrderManagement.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Database - SQSERVERc
+builder.Services.AddDbContext<OrderManagementDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("OrderManagementDb")));
+
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
