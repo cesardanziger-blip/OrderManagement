7ebaa7e9e928da5c9daa5c51fba824e1294d1578
cesardanziger-blip <cesar.danziger@gmail.com>
2026-07-01 20:30:58 -0300

    feat(infrastructure): configure Swagger and DbContext

--- src/OrderManagement.API/Program.cs
+++ src/OrderManagement.API/Program.cs
@@ -4,18 +4,50 @@ using OrderManagement.Application.Services;
 using OrderManagement.Domain.Interfaces;
 using OrderManagement.Infrastructure.Context;
 using OrderManagement.Infrastructure.Repositories;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Database - SQSERVERc
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+// Database
 builder.Services.AddDbContext<OrderManagementDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("OrderManagementDb")));
 
+// Swagger
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFilename);
+
+    options.IncludeXmlComments(xmlPath);
+});
+
+builder.Services.AddSwaggerGen();
+
+// DI
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 builder.Services.AddScoped<ICustomerService, CustomerService>();
+//builder.Services.AddScoped<IProductRepository, ProductRepository>();
+//builder.Services.AddScoped<IProductService, ProductService>();
+//builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+//builder.Services.AddScoped<IOrderService, OrderService>();
 
 var app = builder.Build();
+
+// Swagger middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSwagger();
+
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
